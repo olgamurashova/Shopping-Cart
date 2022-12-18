c4edf72b93229ecc8a18d9764461f4c59c11ba17
Olga Murashova <om.dreyfus@gmail.com>
2022-12-18 14:44:26 -0500

    main.js update cart quantity

--- main.js
+++ main.js
@@ -35,6 +35,13 @@ function ready(){
         button.addEventListener("click", removeCartItem)
     }
 
+    //Quantity changes 
+    var quantityInputs = document.getElementsByClassName('cart-quantity');
+    for (let i = 0; quantityInputs.length < 0; i++) {
+        var input = quantityInputs[i];
+        input.addEventListener("change", quantityChanged);
+    }
+
 }
 
 //Remove Items from Cart 
@@ -42,23 +49,37 @@ function ready(){
 function removeCartItem(e) {
     let buttonClicked = e.target;
     buttonClicked.parentElement.remove();
+    updatetotal(); 
+
+}
+
+//Quantity Changed 
+
+function quantityChanged(e) {
+    var input = e.target;
+    if (isNaN(input.value) || input.value <= 0){
+        input.value = 1;
+
+    }
+
     updatetotal();
+
 }
 
 //Update Total
 
  function updatetotal(){
-    var cartContent = document.getElementsByClassName('.cart-content')[0];
-    var cartBoxes = document.getElementsByClassName('.cart-box');
+    var cartContent = document.getElementsByClassName('cart-content')[0];
+    var cartBoxes = document.getElementsByClassName('cart-box');
     total = 0;
     for( let i = 0; i < cartBox.length; i++) {
         var cartBox = cartBoxes[i];
-        var priceEl = cartBox.getElementsByClassName('.cart-price')[0];
-        var quantityEl = cartBox.getElementsByClassName('.cart-quantity')[0];
+        var priceEl = cartBox.getElementsByClassName('cart-price')[0];
+        var quantityEl = cartBox.getElementsByClassName('cart-quantity')[0];
         var price = parseFloat(priceEl.innerText.replace("$", ""));
         var quantity = quantityEl.value;
         total = total + (price * quantity); 
     }
 
-    document.getElementsByName('.total-price')[0].innerText = "$" + total;
+    document.getElementsByName('total-price')[0].innerText = "$" + total;
  }
